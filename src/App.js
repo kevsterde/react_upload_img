c2f7246f8b85702ac76a8b0932bfbcc72272f186
KevinW Revalde <kevinwebsterr2016@gmail.com>
2023-10-15 01:37:57 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,40 @@
 import './App.css';
 import { useState } from 'react'
-import axios from 'axios'
+import Button from "react-bootstrap/Button"
+import InputGroup from "react-bootstrap/InputGroup"
+import Form from "react-bootstrap/Form"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { storage } from './firebase'
+import { ref, uploadBytes } from 'firebase/storage'
+import { v4 } from 'uuid'
 
 function App() {
-  const [files, setFiles] = useState(null);
-  const [progress, setProgress] = useState({ started: false, pc: 0 });
-  const [msg, setMsg] = useState(null);
+
+  const [imageUpload, setImageUpload] = useState(null);
 
 
-  const handleUpload = () => {
-    console.log(files);
-    if (!files) { setMsg("No File Selected"); return; }
+  const uploadImg = () => {
+    if (imageUpload == null) return;
 
-    const fd = new FormData();
+    const imgRef = ref(storage, `images/${imageUpload.name + v4()}`);
 
-    for (let i = 0; i < files.length; i++) {
-      fd.append(`file${i + 1}`, files[i]);
-    }
-
-
-    setMsg("Uploading...");
-
-    fetch('https://httpbin.org/post', {
-      method: "POST",
-      body: fd,
-      headers: { "Custom-Header": "value", }
+    uploadBytes(imgRef, imageUpload).then(() => {
+      alert("images uploaded");
     })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error("Bad Response")
-        }
-        setMsg("Upload sucessful")
-        return res.json();
-      })
-      .then(data => console.log(data))
-      .catch(error => {
-        setMsg("Upload failed")
-        console.error(error)
-      });
 
-
-
-    // axios.post('https://httpbin.org/post', fd, {
-    //   onUploadProgress: (progressEvent) => {
-    //     setProgress(prevState => { return { ...prevState, pc: progressEvent.progress * 100 } })
-    //   },
-    //   headers: { "Custom-Header": "value", }
-    // })
-    //   .then(res => {
-    //     setMsg("Upload sucessful")
-    //     console.log(res.data)
-    //   })
-    //   .catch(error => {
-    //     setMsg("Upload failed")
-    //     console.error(error)
-    //   });
   }
+
   return (
 
     <div className="App" >
       <h1>Uploading Files in React</h1>
-      <input onChange={(e) => { setFiles(e.target.files) }} type="file" multiple accept='images/png , image/jpeg, images/webp' />
 
-      <button onClick={handleUpload}>Upload</button>
-
-
-      {msg && <span>{msg}</span>}
-
+      <Form>
+        <Form.Group>
+          <Form.Control type="file" onChange={(e) => setImageUpload(e.target.files[0])} />
+        </Form.Group>
+      </Form>
+      <Button variant="outline-primary" onClick={() => uploadImg()} >Upload</Button>
 
     </div >
   );
